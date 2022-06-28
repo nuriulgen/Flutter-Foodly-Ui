b4e941d5c0b2deecf2633c8826a9cc409aefc04c
nuriulgen <bazid04ulgen@gmail.com>
2022-06-28 03:15:13 +0300

    Projenin Color setleri bir class içerisinde toplatıldı.

---
+++ lib/product/color/color_items.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorItems {
+  final Color mainTextColor = const Color(0xFF010F07);
+  final Color bodyTextColor = const Color(0xFF868686);
+  final Color activeColor = const Color(0xFF22A45D);
+  final Color whiteColor = Colors.white;
+}
